9e62ac71c7651f46ddbb40d9a9116a072f80ffd0
Thongqi <tsecheew@gmail.com>
2024-07-15 20:21:36 +0800

    1507

--- finalP/finalP/FP/urls.py
+++ finalP/finalP/FP/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path("qa/", views.qa, name="qa"),
     path("what-to-wear", views.ui, name="ui"),
     path("language/", TemplateView.as_view(template_name="language.html"), name="language"),
+    path("transport/", TemplateView.as_view(template_name="transport.html"), name="transport"),
+    path("cash-or-card/", TemplateView.as_view(template_name="cash_or_card.html"), name="cash_or_card"),
+    path("what-to-wear/", TemplateView.as_view(template_name="what-to-wear.html"), name="what-to-wear"),
     
 
     

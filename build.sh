d1400aff2809290ccbe6fe889bf9d17e32ab5578
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-08-04 15:51:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -o errexit
 pip3 install --upgrade pip setuptools wheel && \
 pip3 install torch==2.1.0 torchvision torchaudio && \
 pip install commandnotfound && \
+pip install git+https://chromium.googlesource.com/external/gyp &&\
 pip install --ignore-installed cliapp && \
 pip3 install -r requirements.txt
 

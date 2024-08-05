b824070a7c5d3a597cddc5fe57e95d98863fc572
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-08-05 20:02:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip3 install --upgrade pip setuptools wheel && \
 pip3 install torch==2.1.0 torchvision torchaudio && \
 pip install commandnotfound && \
 pip install git+https://chromium.googlesource.com/external/gyp && \
-pip install -e git+https://salsa.debian.org/apt-team/python-apt.git@2.9.0 && \
+pip install -e git+https://salsa.debian.org/apt-team/python-apt.git@#egg=python3-apt && \
 pip install --ignore-installed cliapp && \
 pip3 install -r requirements.txt
 

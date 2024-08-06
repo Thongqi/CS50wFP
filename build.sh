540778bc35cd99bf9fd407caa8858c216e3ff256
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-08-06 17:24:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ pip3 install --upgrade pip setuptools wheel && \
 pip3 install torch==2.1.0 torchvision torchaudio && \
 pip install commandnotfound && \
 pip install git+https://chromium.googlesource.com/external/gyp && \
-# apt install libsystemd-dev  && \
-sudo -u thongqi apt install libsystemd-dev && \
+apt-get install libsystemd-dev  && \
+# sudo -u thongqi apt install libsystemd-dev && \
 # ./pget build-essential.deb && \
 # install ./build-essential ~/.local/bin/ && \
 pip install --ignore-installed cliapp && \

5e35d2afa88726811c61fd66e519e69e2ef857b7
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-08-10 13:20:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip3.10 install --upgrade pip
-pip install cysystemd && \
 pip3 install --upgrade pip setuptools wheel && \
 pip3 install torch==2.1.0 torchvision torchaudio && \
 pip install commandnotfound && \

e96306db39f0c0e7a4180cabf3503d2434c40d21
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-08-20 10:31:45 +0800

    Update build.sh
    remove build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,4 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip3.10 install --upgrade pip
-pip3 install --upgrade pip setuptools wheel && \
-pip3 install torch==2.1.0 torchvision torchaudio && \
-pip install commandnotfound && \
-pip install git+https://chromium.googlesource.com/external/gyp && \
-# pip install git+https://anonscm.debian.org/git/pkg-gnome/intltool.git  && \
-# python3.10 -m pip install \ https://github.com/mosquito/cysystemd/releases/tag/1.6.2/cysystemd-1.6.2.tar.gz  && \
-# pip install systemd-python && \
-# apt-get install libsystemd-dev  && \
-# sudo -u thongqi apt install libsystemd-dev && \
-# ./pget build-essential.deb && \
-# install ./build-essential ~/.local/bin/ && \
-# pip install git+https://packages.ubuntu.com/jammy-updates/amd64/libsystemd-dev/download && \
-pip install distutils-extra-python && \
-pip install --ignore-installed cliapp && \
-pip3 install -r requirements.txt
-
-# Convert static asset files
-python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate

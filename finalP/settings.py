65462d529c887c6710dfd0d91daa01145988230d
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-09-02 16:15:08 +0800

    Update settings.py
    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)

--- finalP/settings.py
+++ finalP/settings.py
@@ -86,12 +86,13 @@ WSGI_APPLICATION = 'finalP.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(        # Replace this value with your local database's connection string.        
-            default='postgresql://postgres:postgres@localhost:5432/mysite',        
-            # conn_max_age=600
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
+        # 'default': dj_database_url.config(        # Replace this value with your local database's connection string.        
+        #     default='postgresql://postgres:postgres@localhost:5432/mysite', 
+        #     # conn_max_age=600
+        #     conn_max_age=600,
+        #     conn_health_checks=True,
+        # )
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
 
 

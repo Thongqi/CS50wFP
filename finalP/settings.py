9f1e5c727aea21ecee781ac4fe88efe53d71902f
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-09-02 15:30:38 +0800

    Update settings.py

--- finalP/settings.py
+++ finalP/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'finalP.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(        # Replace this value with your local database's connection string.        
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)        # Replace this value with your local database's connection string.        
             # default='postgresql://postgres:postgres@localhost:5432/mysite',        
             # conn_max_age=600
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
+        #     conn_max_age=600,
+        #     conn_health_checks=True,
+        # )
 }
 
 

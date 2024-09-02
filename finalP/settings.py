0de24aab4f97566503e202014d31a0aa2f335aae
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-09-02 15:04:55 +0800

    Update settings.py
                default= 'postgresql://mysite:tnGdj5PjRjLME9SHVQGa9G1gETgmlMmY@dpg-cqmue4g8fa8c73aidhug-a/mysite_r3sn',

--- finalP/settings.py
+++ finalP/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         'default': dj_database_url.config(        # Replace this value with your local database's connection string.        
             # default='postgresql://postgres:postgres@localhost:5432/mysite',        
             # conn_max_age=600
+            default= 'postgresql://mysite:tnGdj5PjRjLME9SHVQGa9G1gETgmlMmY@dpg-cqmue4g8fa8c73aidhug-a/mysite_r3sn',
             conn_max_age=600,
             conn_health_checks=True,
         )

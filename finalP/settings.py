7cdaf302958521112653e43e2864864708c1031e
Thongqi <141812298+Thongqi@users.noreply.github.com>
2024-09-02 14:42:38 +0800

    Update settings.py DATABASES URL
    DATABASES URL
    conn_max_age=600,
                conn_health_checks=True,

--- finalP/settings.py
+++ finalP/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'finalP.wsgi.application'
 
 DATABASES = {
         'default': dj_database_url.config(        # Replace this value with your local database's connection string.        
-            default='postgresql://postgres:postgres@localhost:5432/mysite',        
-            conn_max_age=600
+            # default='postgresql://postgres:postgres@localhost:5432/mysite',        
+            # conn_max_age=600
+            conn_max_age=600,
+            conn_health_checks=True,
         )
 }
 
